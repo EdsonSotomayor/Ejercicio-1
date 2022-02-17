e9af7dc204caef43973c06be1ac006b780088e6d
EdsonSotomayor <you@example.com>
2022-02-17 11:40:14 -0600

    v1

---
+++ resources/js/app.js
@@ -0,0 +1,36 @@
+const app = {
+    urlDatos : "/resources/data/autos.json",
+
+    cargarFichas : function()
+    {
+        const fichas = document.querySelector("#fichas");
+        let html = "";
+
+        fetch(this.urlDatos)
+            .then( response => response.json())
+            
+            .then( autos => {
+                for( let auto of autos)
+                {
+                    html += `
+                    <div class="ficha">
+                        <img src="/resources/img/${auto.foto}" alt="${auto.foto}">
+                        <div class="datos">
+                            <h3>${auto.marca}</h3>
+                            <span>${auto.modelo}</span>
+                            <span>${auto.ano}</span>
+                            <br>
+                            <small>
+                                ${auto.motor.desplazamiento},
+                                ${auto.motor.potencia},
+                                ${auto.motor.rendimiento},
+                            </small>
+                        </div>
+                        `;
+                }
+                fichas.innerHTML = html
+
+            } ).catch(error => console.error( error ));
+
+    }
+}
